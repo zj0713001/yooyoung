52262a8c7a3a9d7cdf90fbf73e8cfe7573c51a54
zhangjian <iron@yooyoung.cn>
2015-01-13 04:39:13 +0800

    fix settings

--- config/initializers/lazyload.rb
+++ config/initializers/lazyload.rb
@@ -1,4 +1,3 @@
 Lazyload::Rails.configure do |config|
-  puts ActionController::Base.helpers.asset_path "/assets/blank.gif"
   config.placeholder = ActionController::Base.helpers.asset_url "blank.gif"
 end
