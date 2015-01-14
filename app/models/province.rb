0dfbf0c59042bfe133e58b3c8451f804aad271cf
zhangjian <iron@yooyoung.cn>
2015-01-15 02:40:12 +0800

    fix cities task

--- app/models/province.rb
+++ app/models/province.rb
@@ -30,8 +30,8 @@ class Province < ActiveRecord::Base
   has_many :hotels, through: :cities
 
   validates :name, presence: true
-  validates :name, uniqueness: { scope: :active }, if: Proc.new { self.active }
+  validates :name, uniqueness: { scope: [:country_id, :active] }, if: Proc.new { self.active }
   validates :chinese, presence: true
-  validates :chinese, uniqueness: { scope: :active }, if: Proc.new { self.active }
+  validates :chinese, uniqueness: { scope: [:country_id,  :active] }, if: Proc.new { self.active }
   validates :country, existence: true
 end
