66763894602929b6c5590adb1823be5573011eac
zhangjian <iron@yooyoung.cn>
2015-01-23 04:24:20 +0800

    *feature: add ga

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-58854016-1' # Master
+  # config.middleware.use Rack::GoogleAnalytics, :tracker => 'UA-58854016-2' # Beta
 end
