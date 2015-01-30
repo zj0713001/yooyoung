d9539de29b3a239dcf17a2c329fec0a78a057f39
zhangjian <iron@yooyoung.cn>
2015-01-30 13:06:48 +0800

    *moveon): change rainbows conf

--- script/reload.sh
+++ script/reload.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 cd /opt/yooyoung/iron
 
+rainbows_pidfile='tmp/pids/rainbows.pid'
+sidekiq_pidfile='tmp/pids/sidekiq.pid'
 rainbows_pid=`cat tmp/pids/rainbows.pid`
 sidekiq_pid=`cat tmp/pids/sidekiq.pid`
 user='rails'
 
-if [ -f $rainbows_pid ]; then
+if [ -f $rainbows_pidfile ]; then
   kill -USR2 $rainbows_pid
 else
   bundle exec rainbows -c config/rainbows.rb -E production -D
 fi
 
-if [ -f $sidekiq_pid ]; then
+if [ -f $sidekiq_pidfile ]; then
   bundle exec sidekiqctl stop $sidekiq_pid
 fi
 pkill -u $user -f 'sidekiq [0-9]'
-bundle exec sidekiq -C config/sidekiq.yml -e production -d
+bundle exec sidekiq -C config/sidekiq.yml -P $sidekiq_pidfile -L log/sidekiq.log -e production -d
 
 while true; do
   sleep 10
--- script/start.sh
+++ script/start.sh
@@ -2,4 +2,4 @@
 
 cd /opt/yooyoung/iron
 bundle exec rainbows -c config/rainbows.rb -E production -D
-bundle exec sidekiq -C config/sidekiq.yml -e production -d
+bundle exec sidekiq -C config/sidekiq.yml -P tmp/pids/sidekiq.pid -L log/sidekiq.log -e production -d
