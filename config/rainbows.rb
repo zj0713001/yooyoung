9b6cd67cbb0effdce2388dafc12cd6e4ea79b5dc
zhangjian <iron@yooyoung.cn>
2015-01-30 12:34:32 +0800

    *moveon): change rainbows conf

--- config/rainbows.rb
+++ config/rainbows.rb
@@ -1,7 +1,7 @@
 working_directory `pwd -L`.chomp
 
 # paths and things
-socket_path = 'tmp/sockets/rainbows.sock'
+socket_path = '/tmp/rainbows.sock'
 pid_path    = 'tmp/pids/rainbows.pid'
 err_path    = 'log/rainbows.error.log'
 out_path    = 'log/rainbows.out.log'
@@ -16,7 +16,7 @@ worker_processes 2
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen File.expand_path(socket_path), backlog: 64
+listen socket_path, backlog: 64
 listen "127.0.0.1:3000"
 
 daemonize = true
@@ -49,4 +49,5 @@ end
 Rainbows! do
   use :ThreadPool
   worker_connections 16
+  client_max_body_size nil
 end
--- script/reload.sh
+++ script/reload.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+cd /opt/yooyoung/iron
 
-PID=`cat tmp/pids/rainbows.pid`
+rainbows_pid=`cat tmp/pids/rainbows.pid`
+sidekiq_pid=`cat tmp/pids/sidekiq.pid`
+user='rails'
 
-# kill -USR2
-kill -USR2 $PID
-# bundle exec rake sidekiq:stop
-# bundle exec rake sidekiq:start
+if [ -f $rainbows_pid ]; then
+  kill -USR2 $rainbows_pid
+else
+  bundle exec rainbows -c config/rainbows.rb -E production -D
+fi
+
+if [ -f $sidekiq_pid ]; then
+  bundle exec sidekiqctl stop $sidekiq_pid
+fi
+pkill -u $user -f 'sidekiq [0-9]'
+bundle exec sidekiq -C config/sidekiq.yml -e production -d
 
 while true; do
   sleep 10
 
-  num=`ps -ef | grep rainbows | grep master | grep $PID | wc -l`
+  rainbows_num=`ps -ef | grep rainbows | grep master | grep $rainbows_pid | wc -l`
 
-  if (( num > 1 )) ; then
-    kill -QUIT $PID
+  if (( rainbows_num > 1 )) ; then
+    kill -QUIT $rainbows_pid
     break
   fi
 done
