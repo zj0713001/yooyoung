012c717797d3808ca4bd99f69f8e3cdfd63688e3
zhangjian <iron@yooyoung.cn>
2015-01-30 12:40:13 +0800

    *moveon): change rainbows conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "7u2r8l.com1.z0.glb.clouddn.com"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.cache_store
