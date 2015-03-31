c7db8accd846ee732aac3cf8042eba24424b6b08
zhangjian <iron@yooyoung.cn>
2015-04-01 03:05:49 +0800

    *fotfix): fix price

--- app/services/package_service.rb
+++ app/services/package_service.rb
@@ -5,7 +5,7 @@ class PackageService
 
   def min_price_by_date(start_date = Time.now.to_date)
     end_date = start_date + 1.month
-    package_prices = PriceService.new(@package).find_all_by_date(start_date, end_day, price_type: :sale_price)
+    package_prices = PriceService.new(@package).find_all_by_date(start_date, end_date, price_type: :sale_price)
     # end_date = package_prices.max{|d| d[:date]}[:date].to_date
     @package.rooms.map do |room|
       room_price = PriceService.new(room).find_all_by_date(start_date, (end_date+@package.days), price_type: :sale_price)
