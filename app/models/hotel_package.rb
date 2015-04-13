fa3fa46f02892ec968e7685f1d6e9d974a548632
zhangjian <iron@yooyoung.cn>
2015-04-13 15:27:11 +0800

    *hotfix): fix hotel package show

--- app/models/hotel_package.rb
+++ app/models/hotel_package.rb
@@ -19,7 +19,7 @@
 class HotelPackage < ActiveRecord::Base
   has_many :photos, as: :target, dependent: :destroy
   has_and_belongs_to_many :rooms, -> { where active: true }, uniq: true
-  has_many :items, ->{ order 'sequence ASC' }, class_name: HotelPackageItem
+  has_many :items, ->{ order('service_day ASC').order('sequence ASC') }, class_name: HotelPackageItem
   has_many :items_with_out_cover_photo, -> { where("`cover_photo_id` IS NULL").order('sequence ASC') }, class_name: HotelPackageItem
   has_many :items_with_cover_photo, -> { where("`cover_photo_id` IS NOT NULL").order('sequence ASC') }, class_name: HotelPackageItem
 
@@ -43,9 +43,11 @@ class HotelPackage < ActiveRecord::Base
 
   after_save :sequence_items
   def sequence_items
-    self.items.each_with_index do |item, index|
-      item.sequence = index + 1
-      item.save
+    self.items.group_by(&:service_day).sort.each_with_index do |(day_index, items), index|
+      items.each_with_index do |item, index|
+        item.sequence = index + 1
+        item.save
+      end
     end
   end
 end
